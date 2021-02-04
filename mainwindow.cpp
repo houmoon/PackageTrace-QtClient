8e36159ddedd1e4fef199b41d0c2b786d55af760
LeeJeongHo <aofhd15@gmail.com>
2021-02-04 09:20:02 +0900

    파이썬 스크립트 연결

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QProcess>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,4 +13,24 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_Button_Refresh_clicked()
+{
+        QString path = QCoreApplication::applicationDirPath();
+        QString  command("python");
+        QStringList params = QStringList() << "tracer.py" << "kr.cjlogistics" << "637197513201";
+
+        QProcess *process = new QProcess();
+
+        if(!process->startDetached(command, params, path))
+        {
+            QMessageBox MsgBox;
+            MsgBox.critical(0,"에러","프로세스를 실행하지 못했습니다.");
+            MsgBox.setFixedSize(500,200);
+            MsgBox.exec();
+        }
+
+        process->waitForFinished();
+        process->close();
 }
